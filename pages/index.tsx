00370dd56aff0e51e3b308f59eedbd4794f7d2b4
Raylouis <sebastianraynard@gmail.com>
2023-07-14 22:24:43 +0700

    feat : add Jumbroton and Description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Jumbroton from "@src/contexts/Jumbroton/Jumbroton"
+import Description from "@src/contexts/Description/Description"
 const Landing: React.FC = () => {
   return (
     <div className='h-screen relative font-[Montserrat-Bold]'>
-      your code here
+      <Jumbroton/>
+      <Description/>
     </div>
   )
 }
