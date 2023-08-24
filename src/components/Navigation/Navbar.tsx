aa0de058d3ce4af3ee3b0d42973cdaadc26a379e
bewe <16521486@mahasiswa.itb.ac.id>
2023-08-24 16:42:01 +0700

    fix: try navbar

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -220,7 +220,7 @@ const Navbar: React.FC = () => {
             <Image src={Logo} alt="logo" className="" />
           </a>
         </div>
-        <div className="h-0 flex fixed z-20">
+        <div className="h-0 flex fixed z-20 right-0 top-0">
           <div
             className={`${
               open ? 'fixed right-0' : ''
