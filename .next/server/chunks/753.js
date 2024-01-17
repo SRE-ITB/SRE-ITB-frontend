79dc877772e21ed2bf2b7cc59311751894447412
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-01-17 23:26:35 +0700

    feat: build next

--- .next/server/chunks/753.js
+++ .next/server/chunks/753.js
@@ -3,130 +3,83 @@ exports.id = 753;
 exports.ids = [753];
 exports.modules = {
 
-/***/ 1753:
-/***/ ((module, __webpack_exports__, __webpack_require__) => {
+/***/ 986:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
-__webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _src_contexts_404_404page__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1004);
-/* harmony import */ var _src_components_SEO_SEO__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5905);
-/* harmony import */ var _src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1841);
-/* harmony import */ var _src_components_Navigation_Footer__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(3406);
-var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__]);
-_src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
-
-
-
-
-
-const ErrorPage = ()=>{
-    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_src_components_SEO_SEO__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {
-        title: "SRE ITB | Error",
-        children: [
-            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
-                e: "error"
-            }),
-            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_src_contexts_404_404page__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {}),
-            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_src_components_Navigation_Footer__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z, {})
-        ]
-    });
-};
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ErrorPage);
-
-__webpack_async_result__();
-} catch(e) { __webpack_async_result__(e); } });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/CareerTalks.8df85dc8.webp","height":664,"width":1118,"blurDataURL":"data:image/webp;base64,UklGRk4AAABXRUJQVlA4IEIAAACwAQCdASoIAAUAAkA4JagC7ADzes2gAP6q6cBfrhVwcT0lwUQjnaYt57AP5HMQrF/zfJ4C/BuLgZr9Ov1v8GdIAAA=","blurWidth":8,"blurHeight":5});
 
 /***/ }),
 
-/***/ 1004:
+/***/ 8004:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/CompanyVisit.0dde8fbb.webp","height":1108,"width":1477,"blurDataURL":"data:image/webp;base64,UklGRk4AAABXRUJQVlA4IEIAAADQAQCdASoIAAYAAkA4JQBOgB6TMZy3TAD4YtQ8XR+4CwDLp/KhfQBIchM9GxOs6Yt5p0U9URle4FJeRVhI55gAAAA=","blurWidth":8,"blurHeight":6});
 
-// EXPORTS
-__webpack_require__.d(__webpack_exports__, {
-  "Z": () => (/* binding */ _404page)
-});
+/***/ }),
 
-// EXTERNAL MODULE: external "react/jsx-runtime"
-var jsx_runtime_ = __webpack_require__(997);
-// EXTERNAL MODULE: external "react"
-var external_react_ = __webpack_require__(6689);
-// EXTERNAL MODULE: ./node_modules/next/image.js
-var next_image = __webpack_require__(5675);
-var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
-;// CONCATENATED MODULE: ./src/assets/Images/Illustration/Plant.svg
-/* harmony default export */ const Plant = ({"src":"/_next/static/media/Plant.6fad3998.svg","height":500,"width":500});
-;// CONCATENATED MODULE: ./src/contexts/404/404page.tsx
+/***/ 1855:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/Gathering.698721eb.webp","height":1600,"width":2400,"blurDataURL":"data:image/webp;base64,UklGRkgAAABXRUJQVlA4IDwAAACwAQCdASoIAAUAAkA4JZACdADze7UQAP7CUh/VZt2IHf4uHI3pQ10lKQe6hHXDlnoItK8XdLCFpqQAAAA=","blurWidth":8,"blurHeight":5});
 
+/***/ }),
 
+/***/ 2504:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-const NotFound = ()=>{
-    const { 0: isWideScreen , 1: setIsWideScreen  } = (0,external_react_.useState)(false);
-    (0,external_react_.useEffect)(()=>{
-        function handleResize() {
-            setIsWideScreen(window.innerWidth > 640);
-        }
-        window.addEventListener("resize", handleResize);
-        handleResize();
-        return ()=>{
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
-        className: "h-screen relative items-center sm:items-start flex flex-col sm:flex-row font-montserrat",
-        children: [
-            /*#__PURE__*/ jsx_runtime_.jsx("div", {
-                className: "w-[80vw] justify-center flex sm:w-[45vw] mt-[75px]",
-                children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
-                    src: Plant,
-                    alt: "plant",
-                    className: ""
-                })
-            }),
-            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
-                className: "flex flex-col items-center sm:items-start sm:w-[55vw] sm:h-screen sm:justify-center",
-                children: [
-                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
-                        className: "text-center sm:text-left px-[5vw] sm:px-0 sm:pr-[5vw]",
-                        children: [
-                            /*#__PURE__*/ jsx_runtime_.jsx("h1", {
-                                className: "text-[30px] md:text-[35px] xl:text-[40px] font-bold font-gradient",
-                                children: isWideScreen ? /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
-                                    children: [
-                                        "Page",
-                                        /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
-                                        "Not Found"
-                                    ]
-                                }) : /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
-                                    children: "Page Not Found"
-                                })
-                            }),
-                            /*#__PURE__*/ jsx_runtime_.jsx("p", {
-                                className: "text-[16px] md:text-[18px] xl:text-[20px] mt-[2vw]",
-                                children: "Oops! It looks like you've stumbled upon a page that doesn't exist. Maybe the page has been moved, deleted, or you entered the wrong URL."
-                            })
-                        ]
-                    }),
-                    /*#__PURE__*/ jsx_runtime_.jsx("button", {
-                        className: "mt-[5vw] border-[2px] font-extrabold border-green5 bg-white hover:bg-green5 rounded-full text-green5 duration-75 hover:text-white w-[275px] md:w-[325px] xl:w-[400px] h-[50px] text-[16px] md:text-[18px] xl:text-[20px]",
-                        onClick: ()=>{
-                            window.location.href = "/";
-                        },
-                        children: "Go Home"
-                    })
-                ]
-            })
-        ]
-    });
-};
-/* harmony default export */ const _404page = (NotFound);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/REview.58c7fc39.webp","height":1257,"width":2169,"blurDataURL":"data:image/webp;base64,UklGRkwAAABXRUJQVlA4IEAAAACQAQCdASoIAAUAAkA4JYgAAuWtZHAA+GEU3ceemFeDt3ErVm5WJH26kd8ieT//ZZ8oe0bvpw2vFNZoAH8AAAAA","blurWidth":8,"blurHeight":5});
 
+/***/ }),
+
+/***/ 5249:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/SkillTraining.4e512e65.webp","height":1600,"width":2400,"blurDataURL":"data:image/webp;base64,UklGRkgAAABXRUJQVlA4IDwAAACQAQCdASoIAAUAAkA4JZwAAppj9YQA/th6IroNY6zW/efLgGvOj7ukx51JTF2OGJ3GkbqEAArKoSMAAAA=","blurWidth":8,"blurHeight":5});
+
+/***/ }),
+
+/***/ 5894:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/REProject.3ebcc01c.webp","height":776,"width":1379,"blurDataURL":"data:image/webp;base64,UklGRkYAAABXRUJQVlA4IDoAAADQAQCdASoIAAUAAkA4JYgCdAEO137PgAD7ngzuoUB77l6u7AlqWawbdOLjMg+m1IAj3YU/Y2+gYYAA","blurWidth":8,"blurHeight":5});
+
+/***/ }),
+
+/***/ 8536:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/SocialProject.67826902.webp","height":959,"width":1705,"blurDataURL":"data:image/webp;base64,UklGRkAAAABXRUJQVlA4IDQAAACwAQCdASoIAAQAAkA4JZQCdAD0gbVuAP7tIZbXwZNCchpIHypexK9UXk58UKzyWYXtAGAA","blurWidth":8,"blurHeight":4});
+
+/***/ }),
+
+/***/ 1555:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/Leaf.5efe5deb.png","height":1060,"width":1060,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAk0lEQVR42mPABUr2aTOW7tdmBHOCp5gwRswyYoJJFu8BSsCAX48JmAMDySsMmGHsisNawmBGyBQTcaAJqglLDNlgkvnbdfqLdutUMPj2mEoFTjKxC59pHBoz3zAJqMgwabkhT85m3cq8bbplDD7dphwwXdFzjeTjFxuap6w0UGEgBhTs0GGCsWEmMAJNYAKaAHc4ANbZJ2EWYoecAAAAAElFTkSuQmCC","blurWidth":8,"blurHeight":8});
 
 /***/ })
 
--- .next/server/pages/_error.js
+++ .next/server/pages/_error.js
@@ -5,6 +5,133 @@ exports.id = 820;
 exports.ids = [820];
 exports.modules = {
 
+/***/ 1753:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _src_contexts_404_404page__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1004);
+/* harmony import */ var _src_components_SEO_SEO__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5905);
+/* harmony import */ var _src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1841);
+/* harmony import */ var _src_components_Navigation_Footer__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(3406);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__]);
+_src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+
+
+
+
+const ErrorPage = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_src_components_SEO_SEO__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {
+        title: "SRE ITB | Error",
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_src_components_Navigation_Navbar__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+                e: "error"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_src_contexts_404_404page__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {}),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_src_components_Navigation_Footer__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z, {})
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ErrorPage);
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 1004:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ _404page)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+;// CONCATENATED MODULE: ./src/assets/Images/Illustration/Plant.svg
+/* harmony default export */ const Plant = ({"src":"/_next/static/media/Plant.6fad3998.svg","height":500,"width":500});
+;// CONCATENATED MODULE: ./src/contexts/404/404page.tsx
+
+
+
+
+const NotFound = ()=>{
+    const { 0: isWideScreen , 1: setIsWideScreen  } = (0,external_react_.useState)(false);
+    (0,external_react_.useEffect)(()=>{
+        function handleResize() {
+            setIsWideScreen(window.innerWidth > 640);
+        }
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return ()=>{
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: "h-screen relative items-center sm:items-start flex flex-col sm:flex-row font-montserrat",
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "w-[80vw] justify-center flex sm:w-[45vw] mt-[75px]",
+                children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                    src: Plant,
+                    alt: "plant",
+                    className: ""
+                })
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: "flex flex-col items-center sm:items-start sm:w-[55vw] sm:h-screen sm:justify-center",
+                children: [
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: "text-center sm:text-left px-[5vw] sm:px-0 sm:pr-[5vw]",
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                className: "text-[30px] md:text-[35px] xl:text-[40px] font-bold font-gradient",
+                                children: isWideScreen ? /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+                                    children: [
+                                        "Page",
+                                        /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                        "Not Found"
+                                    ]
+                                }) : /*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+                                    children: "Page Not Found"
+                                })
+                            }),
+                            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                                className: "text-[16px] md:text-[18px] xl:text-[20px] mt-[2vw]",
+                                children: "Oops! It looks like you've stumbled upon a page that doesn't exist. Maybe the page has been moved, deleted, or you entered the wrong URL."
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("button", {
+                        className: "mt-[5vw] border-[2px] font-extrabold border-green5 bg-white hover:bg-green5 rounded-full text-green5 duration-75 hover:text-white w-[275px] md:w-[325px] xl:w-[400px] h-[50px] text-[16px] md:text-[18px] xl:text-[20px]",
+                        onClick: ()=>{
+                            window.location.href = "/";
+                        },
+                        children: "Go Home"
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const _404page = (NotFound);
+
+
+/***/ }),
+
 /***/ 3280:
 /***/ ((module) => {
 
@@ -257,7 +384,7 @@ module.exports = import("framer-motion");;
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [675,676,664,548,406,753], () => (__webpack_exec__(1753)));
+var __webpack_exports__ = __webpack_require__.X(0, [675,676,664,548,406], () => (__webpack_exec__(1753)));
 module.exports = __webpack_exports__;
 
 })();
