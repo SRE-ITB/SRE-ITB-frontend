bf9a7651bc97e9f5fe29453c1f74a14ed63643b3
Julian Caleb Simandjuntak <juliancaleb88@gmail.com>
2024-02-05 09:48:01 +0700

    feat: Compe Footer

--- pages/iyref/compe/bcc/index.tsx
+++ pages/iyref/compe/bcc/index.tsx
@@ -1,12 +1,20 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
 import SEO from '@src/components/SEO/SEO'
 import Navbar from '@src/components/Navigation/Navbar'
 import Comingsoon from '@src/contexts/ComingSoon/ComingSoon'
+import FooterCompe from '@src/contexts/IYREF/Compe/FooterCompe'
 
 const BCCPage = (): JSX.Element => {
   return (
     <SEO title="SRE ITB | BCC">
       <Navbar />
       <Comingsoon />
+      <FooterCompe
+        contacts={[
+          'Jessica (id line : jessicaabdiman)',
+          'Andrea (WA : 087776108222)',
+        ]}
+      />
     </SEO>
   )
 }
