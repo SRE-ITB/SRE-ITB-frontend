d9890fc78626242a07f8f76d10ccb67e5d065dd5
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-02-08 19:42:04 +0700

    fix: timeline and stages structure

--- pages/iyref/compe/bcc/index.tsx
+++ pages/iyref/compe/bcc/index.tsx
@@ -2,8 +2,31 @@
 import SEO from '@src/components/SEO/SEO'
 import Navbar from '@src/components/Navigation/Navbar'
 import Comingsoon from '@src/contexts/ComingSoon/ComingSoon'
+import TImeline from '@src/contexts/IYREF/Compe/Timeline'
+import Stages from '@src/contexts/IYREF/Compe/Stages'
 import FooterCompe from '@src/contexts/IYREF/Compe/FooterCompe'
 import Events from '@src/contexts/IYREF/Compe/Event'
+
+interface TimelineProps {
+  timeline: LineProps[]
+}
+
+interface LineProps {
+  title: string
+  desc: string
+}
+
+interface CompeProps {
+  title: string
+  desc: string
+  cards: CardProps[]
+}
+
+interface CardProps {
+  title: string
+  subtitle: string
+  desc: string
+}
 
 interface WebinarProps {
   title: string
@@ -26,6 +49,69 @@ interface CoachingProps {
 }
 
 const BCCPage = (): JSX.Element => {
+  const timeline: TimelineProps = {
+    timeline: [{
+      title: '24 Feb - 9 Mar 2024',
+      desc: 'Open Registration'
+    },
+    {
+      title: '9 March 2024',
+      desc: 'Free Webinar'
+    },
+    {
+      title: '17 March 2024',
+      desc: 'Case Distribution'
+    },
+    {
+      title: '17 - 31 March 2024',
+      desc: 'Preliminary Phase (Executive Summary)'
+    },
+    {
+      title: '17 - 19 April 2024',
+      desc: 'Semifinal Registration'
+    },
+    {
+      title: '17 Apr - 1 May 2024',
+      desc: 'Semifinal Phase (Proposal)'
+    },
+    {
+      title: '28 April 2024',
+      desc: 'Coaching 1'
+    },
+    {
+      title: '10 - 17 May 2024',
+      desc: 'Final Phase (Pitch Deck)'
+    },
+    {
+      title: '15 May 2024',
+      desc: 'Coaching 2'
+    },
+    {
+      title: '18 May 2024',
+      desc: 'Pitching and Awarding'
+    }]
+  }
+  const stages: CompeProps = {
+    title: 'Business Case Competition',
+    desc: 'The IYREF Business Case Competition 2024 is divided into three main stages: Preliminary, Semifinal, and Final Stage. All participants in the respective stages are required to complete and submit documents or other necessary items at each stage.',
+    cards: [
+      {
+        title: 'Registration and Preliminary Stage',
+        subtitle: 'Executive Summary Submission',
+        desc: 'In this stage, participants are required to create an Executive Summary containing a brief introduction or overview of the details regarding the issues and solutions identified in the given case. Participants are encouraged to provide their initial comprehensive perspectives and insights into their solutions. Participants are advised to write the Executive Summary according to the applicable guidelines.'
+      },
+      {
+        title: 'Re-registration and Semifinal Stage ',
+        subtitle: 'Proposal Submission',
+        desc: 'Teams that qualify for the semifinal stage are required to create a Proposal that includes detailed explanations and elaborations of the issues and solutions they have presented during the preliminary round. This phase entails a more in-depth exploration of the specific details of the problem and the solutions offered by the participants. Each team that advances to the semifinal stage is subject to a registration fee of Rp150.000, with the payment period starting from April 17 to April 19, 2024.'
+      },
+      {
+        title: 'Final Stage',
+        subtitle: 'Pitch Deck Submission & Pitching',
+        desc: 'The top 5 teams with the highest scores will progress to the final stage and are required to submit a pitch deck as a pitching aid during the presentation by the 5 finalists. The pitching session will be conducted offline at the Ganesha campus of the Bandung Institute of Technology, and the top 5 finalists must be present on May 18, 2024.'
+      }
+    ],
+  }
   const webinar: WebinarProps = {
     title:
       'Webinar “Navigating BCC Strategies for Successful Pitch Deck Development”',
@@ -51,6 +137,8 @@ const BCCPage = (): JSX.Element => {
     <SEO title="SRE ITB | BCC">
       <Navbar />
       <Comingsoon />
+      <TImeline timeline={timeline.timeline} />
+      <Stages stages={stages} />
       <Events webinar={webinar} coaching={coaching} />
       <FooterCompe
         contacts={[
---
+++ src/contexts/IYREF/Compe/Timeline.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image'
+import PropTypes from 'prop-types'
+import Pot from '@src/assets/Images/IYREF/Compe/Timeline/pot.svg'
+
+interface TimelineProps {
+  timeline: LineProps[]
+}
+
+interface LineProps {
+  title: string
+  desc: string
+}
+
+const Timeline = ({ title, desc }: LineProps): JSX.Element => {
+  return (
+      <div className='flex mt-[3.6vh]'>
+          <div className='absolute ml-[0.8vw] mt-[-10vh] xl:mt-[-12vh] bg-green8 h-[12vh] xl:h-[16vh] w-[0.2vw]'></div>
+          <div className='mt-[1.3vh] h-[1.8vw] aspect-square bg-green8 rounded-full'></div>
+          <div className='ml-[3vw]'>
+              <p className='text-transparent font-bold bg-clip-text bg-green8 text-[18px] xl:text-[24px]'>{title}</p>
+              <p className='text-transparent bg-clip-text bg-green1 text-[16px] xl:text-[22px]'>{desc}</p>
+          </div>
+      </div>
+  )
+}
+Timeline.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired
+}
+
+const IYREF = ({ timeline }: TimelineProps): JSX.Element => {
+  return (
+    <div className='overflow-x-hidden font-montserrat'>
+      <div className="xl:grid grid-cols-2 gap-[8vw] p-[5vw] pb-0">
+        <p className="row-start-1 col-start-1 text-transparent font-bold bg-clip-text ml-auto xl:mr-[3vw] text-center bg-gradient-to-r from-green1 to-green8 text-[36px] md:text-[64px] xl:text-[82px]">Timeline</p>
+        <Image
+          className='col-start-1 h-[60vw] mt-[25vw] row-start-1 hidden xl:block'
+          src={Pot}
+          alt="Pot-Image"
+        />
+        <div className='row-start-1 col-start-2 mt-[4vh] xl:mt-0 ml-[5vw] xl:ml-0'>
+            {timeline.map((line, index) => (
+              <div key={index} className='flex mt-[3.6vh]'>
+                <div className='absolute ml-[0.8vw] mt-[-10vh] xl:mt-[-12vh] bg-green8 h-[12vh] xl:h-[16vh] w-[0.2vw]'></div>
+                <div className='mt-[1.3vh] h-[1.8vw] aspect-square bg-green8 rounded-full'></div>
+                <div className='ml-[3vw]'>
+                  <p className='text-transparent font-bold bg-clip-text bg-green8 text-[18px] xl:text-[24px]'>{line.title}</p>
+                  <p className='text-transparent bg-clip-text bg-green1 text-[16px] xl:text-[22px]'>{line.desc}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+IYREF.propTypes = {
+  timeline: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired
+  })).isRequired
+}
+
+export default IYREF
