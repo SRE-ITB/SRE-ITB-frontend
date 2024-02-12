0faf259e0ce07405cdf9c39b338c7bf8e624a98f
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-02-13 00:48:41 +0700

    fix: routing navbar

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -43,7 +43,7 @@ const MENU_LIST: Menu[] = [
   },
   {
     text: 'Competitions',
-    href: '/iyref/compe/bcc',
+    href: '/iyref/bcc',
     contents: [
       {
         text: 'Business Case Competition',
